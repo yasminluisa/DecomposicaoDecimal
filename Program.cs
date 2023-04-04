89de6a57302236ca3dfeec7eb20e6e83c308a7c7
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-04-04 08:25:14 -0300

    decomposiçaõ do número 1580

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("--- Decomposição Decimal ---\n");
+
+Console.Write("Digite um número inteiro...: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int restante = numero;
+
+int unidades = restante % 10;
+restante /= 10;
+
+int dezenas = restante % 10;
+restante /= 10;
+
+int centenas = restante;
+
+Console.WriteLine($"\nO número {numero} possui:");
+Console.WriteLine($"{unidades,10} unidade(s)");
+Console.WriteLine($"{dezenas,10} dezenas(s)");
+Console.WriteLine($"{centenas,10} centena(s).");
